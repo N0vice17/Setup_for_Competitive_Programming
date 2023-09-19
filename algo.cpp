e9292e520e8e105ebadddbc471ffbddad2c4adca
Debojit Ganguly <97492549+N0vice17@users.noreply.github.com>
2023-09-19 12:49:35 +0530

    Update algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -1,53 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long int
-int m=1e9+7;
-int euclid_gcd(int a,int b){
-	if(b==0){
-		return a;
-	}
-	return euclid_gcd(b,a%b);
+#define ll long long int
+ll m=1e9+7;
+ll euclid_gcd(ll a,ll b){
+    if(b==0){
+        return a;
+    }
+    return euclid_gcd(b,a%b);
 }
-bool sieve(int n){
-	vector<bool>arr(n+1,1);
-	arr[0]=0;
-	arr[1]=0;
-	for(int i=2;i*i<=n;i++){
-		if(arr[i]){
-			for(int j=i*2;j<=n;j+=i){
-				arr[j]=0;
-			}
-		}
-	}
-	return arr[n];
+vector<bool> sieve(ll n){
+    vector<bool>arr(n+1,1);
+    arr[0]=0;
+    arr[1]=0;
+    for(int i=2;i*i<=n;i++){
+        if(arr[i]){
+            for(int j=i*2;j<=n;j+=i){
+                arr[j]=0;
+            }
+        }
+    }
+    return arr;
 }
-int modulo_add(int a,int b,int m){
-	return (a % m + b % m) % m;
+ll mod_add(ll a,ll b,ll m){
+    return (a % m + b % m) % m;
 }
-int modulo_minus(int a,int b,int m){
-	return (a % m - b % m) % m;
+ll mod_minus(ll a,ll b,ll m){
+    return (a % m - b % m) % m;
 }
-int modulo_mul(int a,int b,int m){
-	return (a % m * b % m) % m;
+ll mod_mul(ll a,ll b,ll m){
+    return (a % m * b % m) % m;
 }
-int binary_expo(int a,int b){
-	int ans=1,base=a;
-	while(b>0){
-		if(b%2==0){
-			base*=base;
-			b=b/2;
-		}
-		else{
-			ans=ans*base;
-			b=b-1;
-		}
-	}
-	return ans;
+ll binary_expo(ll a,ll b){
+    int ans=1,base=a;
+    while(b>0){
+        if(b%2==0){
+            base*=base;
+            b=b/2;
+        }
+        else{
+            ans=ans*base;
+            b=b-1;
+        }
+    }
+    return ans;
 }
-int modulo_div(int a,int b,int m){
-	return modulo_mul(a,binary_expo(b,m-2),m);
+ll modulo_div(ll a,ll b,ll m){
+    return mod_mul(a,binary_expo(b,m-2),m);
 }
 signed main(){
-	cout<<"hello world"<<endl;
-	return 0;
+    cout<<"hello world"<<endl;
+    return 0;
 }
